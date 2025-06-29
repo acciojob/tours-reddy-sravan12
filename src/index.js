e4b06a3e961525db39a19ae024e10983462b0f60
reddy-sravan12 <sravankumarreddy102@gmail.com>
2025-06-29 19:56:26 +0530

    hiiiiiiiii

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import 'regenerator-runtime/runtime';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-ReactDOM.render(
-  <div>
-    
-      <App />
-    
-  </div>,
-  document.getElementById("root")
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
